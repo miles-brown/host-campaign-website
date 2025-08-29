d121da68988a4f5aa3e89d0bad0fb304ec52c5fe
miles-brown <120441173+miles-brown@users.noreply.github.com>
2025-08-29 22:16:17 +0100

    fix: configure Vite and Tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 /* eslint-env node */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
 // https://vitejs.dev/config/
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
